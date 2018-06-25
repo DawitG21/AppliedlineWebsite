2e46359f7525fa08019eaf8256fff55479805986
Adeleke Adelaja <adelekea@appliedline.com>
2018-06-25 21:55:13 +0300

    update api url

--- src/providers/api.provider.ts
+++ src/providers/api.provider.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ApiProvider {
-	readonly apiServer = 'http://192.168.0.101/'; // 'https://appliedline.com/'; // ends with /
+	readonly apiServer = 'https://appliedline.com/';
 	readonly apiBaseUrl = this.apiServer + 'appliedlineapi/';
 
 	readonly apiUrl = this.apiBaseUrl + 'api/v1/';
