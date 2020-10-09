80032549f240466d5a2e4e97415bd53189b7fe4d
Adeleke Adelaja <AdelekeA@appliedline.com>
2020-10-09 13:22:55 +0000

    Merged PR 355: rxjs clean up
    - #461 refactor rxjx import
    - remove console log
    - remove rxjs-compat
    - Merge branch 'rxjs'
    
    Related work items: #461

--- src/providers/contact.service.provider.ts
+++ src/providers/contact.service.provider.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-// import { BehaviorSubject } from 'rxjs/Rx';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable()
 export class ContactServiceProvider {
--- src/providers/webrequest.provider.ts
+++ src/providers/webrequest.provider.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ApiProvider } from './api.provider';
 import { IWebContact } from '../interfaces/webcontact.interface';
-import { Observable } from 'rxjs-compat/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/timeout';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/internal/operators/catchError';
+import { map } from 'rxjs/internal/operators/map';
+import { throwError } from 'rxjs';
+import { timeout } from 'rxjs/internal/operators/timeout';
 
 /**
  *
@@ -21,14 +22,11 @@ export class WebRequestProvider {
 	sendRequest(contact: IWebContact): Observable<IWebContact> {
 		const url = this.api.apiUrl + this.routeContact;
 
-		console.log(url);
 		return this.http.post(url, contact)
-			.timeout(10000)
-			.map((response) => <IWebContact>response)
-			.catch(this.handleError);
+			.pipe(map((response: any) => response), catchError(this.handleError), timeout(15000));
 	}
 
 	private handleError(error: Response) {
-		return Observable.throw(error);
+		return throwError(error);
 	}
 }
